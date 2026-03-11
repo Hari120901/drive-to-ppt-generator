9d01c04e3d3b3a5de072e43e77a9a87b827ccd56
Hari120901 <hari@adonmo.com>
2026-03-11 17:47:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -172,7 +172,7 @@ if generate_btn:
                 p.font.name = "Montserrat"
                 p.font.bold = True
                 p.font.color.rgb = TEAL
-                p.alignment = PP_ALIGN.CENTER
+                p.alignment = PP_ALIGN.LEFT
 
                 # -------------------------
                 # Add Images
